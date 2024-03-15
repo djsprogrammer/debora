7e618bc3756b3c608a4b0c66dfbf6930ff1efb6a
Deivid de Jesus <imdeividsilva@gmail.com>
2024-03-15 11:11:55 -0300

    remoção da data atual

--- src/components/forms/DateInput.tsx
+++ src/components/forms/DateInput.tsx
@@ -1,24 +1,13 @@
-import { getCurrentDate } from 'formFunctions/common'
-import { useEffect, useRef } from 'react'
-
 interface DateInputProps {
     setDate: React.Dispatch<React.SetStateAction<string>>
 }
 
 const DateInput = ({ setDate }: DateInputProps) => {
 
-    const dateInput = useRef<HTMLInputElement>(null)
-
-    useEffect(() => {
-        if (dateInput.current) {
-            dateInput.current.value = getCurrentDate()
-        }
-    }, [])
-
     return (
         <div className='input-group'>
             <label className='input-group-text'>Escolha uma data</label>
-            <input ref={dateInput} onChange={e => setDate(e.target.value)} className='pe-1 form-control text-center' type='date' required />
+            <input onChange={e => setDate(e.target.value)} className='pe-1 form-control text-center' type='date' required />
         </div>
     )
 
