29c216101d752104f612a4399b8dcf20d56b0e22
Deivid de Jesus <imdeividsilva@gmail.com>
2024-03-16 12:41:42 -0300

    DateInput - usando a data atual como valor padrão

--- src/components/forms/DateInput.tsx
+++ src/components/forms/DateInput.tsx
@@ -1,13 +1,25 @@
+import { getCurrentDate } from "formFunctions/common"
+import { useEffect, useRef } from "react"
+
 interface DateInputProps {
     setDate: React.Dispatch<React.SetStateAction<string>>
 }
 
 const DateInput = ({ setDate }: DateInputProps) => {
 
+    const input = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        if (input.current) {
+            input.current.value = getCurrentDate()
+            setDate(getCurrentDate)
+        }
+    }, [])
+
     return (
         <div className='input-group'>
             <label className='input-group-text'>Escolha uma data</label>
-            <input onChange={e => setDate(e.target.value)} className='pe-1 form-control text-center' type='date' required />
+            <input ref={input} onChange={e => setDate(e.target.value)} className='pe-1 form-control text-center' type='date' required />
         </div>
     )
 
